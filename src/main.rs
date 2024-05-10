4c9adc8d36ed847ddae023be40e0fc1917e8c12a
rajneesh.saini <rajneesh.saini@motional.com>
2024-05-10 15:07:35 -0700

    next step added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,32 @@
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+
+fn handle_client(stream: TcpStream) {
+    let mut stream = stream;
+    let mut data: [u8; 1500] = [0; 1500];
+    // read data from socket
+    match stream.read(&mut data) {
+        Ok(_n) => {
+            println!("Received {_n} bytes from {}", stream.peer_addr().unwrap());
+            let command = String::from_utf8(data[0.._n].to_vec());
+            if command.is_ok() {
+                let command = command.unwrap();
+                println!("Incomign command: {command}");
+                if command == "PING\r\n".to_string() {
+                    let _ = stream.write("PONG\r\n".to_string().as_bytes());
+                }
+            } else {
+                println!("not sure what we received...");
+            }
+        }
+        Err(e) => println!(
+            "Error reading from socket: {:?}: {}",
+            stream.peer_addr().unwrap(),
+            e
+        ),
+    }
+    let _ = stream.shutdown(Shutdown::Both);
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,6 +39,7 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_client(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
