378b761bcac7bced3a57ba83a909dac88e267604
rajneesh.saini <rajneesh.saini@motional.com>
2024-05-13 22:58:47 -0700

    add support for master_replid and offset (hardcoded for now)

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -21,14 +21,28 @@ pub fn handler(
         }
     }
     if replication {
+        let mut master = false;
         let mut role = "role:slave";
         if db.role_master() {
+            master = true;
             role = "role:master";
         }
         let _ = std::fmt::write(
             &mut response,
             format_args!("${}\r\n{}\r\n", role.len(), role),
         );
+        if master {
+            let repl_id = "master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+            let _ = std::fmt::write(
+                &mut response,
+                format_args!("${}\r\n{}\r\n", repl_id.len(), repl_id),
+            );
+            let offset = "master_repl_offset:0";
+            let _ = std::fmt::write(
+                &mut response,
+                format_args!("${}\r\n{}\r\n", offset.len(), offset),
+            );
+        }
     }
     stream.write_all(response.as_bytes())
 }
