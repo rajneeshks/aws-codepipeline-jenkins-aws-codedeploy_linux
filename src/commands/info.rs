4d7d32d9a8de58ade49837abdaee151a84ba57a8
rajneesh.saini <rajneesh.saini@motional.com>
2024-05-13 22:19:53 -0700

    add info replication command support - response is hardcoded

---
+++ src/commands/info.rs
@@ -0,0 +1,31 @@
+use crate::commands::array;
+use crate::commands::resp;
+use crate::store::db;
+use std::io::Write;
+use std::net::TcpStream;
+use std::sync::Arc;
+
+pub const COMMAND_NAME: &str = "info";
+
+pub fn handler(
+    cmd: &resp::DataType,
+    stream: &mut TcpStream,
+    _db: &Arc<db::DB>,
+) -> std::io::Result<()> {
+    // only be called when data type is appropriate
+    let mut response = String::new(); //("*\r\n");
+    let mut replication = false;
+    if let Some(info_type) = array::get_nth_arg(cmd, 1) {
+        if info_type.contains("replication") {
+            replication = true;
+        }
+    }
+    if replication {
+        let role = "role:master";
+        let _ = std::fmt::write(
+            &mut response,
+            format_args!("${}\r\n{}\r\n", role.len(), role),
+        );
+    }
+    stream.write_all(response.as_bytes())
+}
