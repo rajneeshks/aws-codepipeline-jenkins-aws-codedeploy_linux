751c62b868b38b8c6dcaee139bf1ca22db15109d
rajneesh.saini <rajneesh.saini@motional.com>
2024-05-14 21:45:38 -0700

    add empty RDB support

--- src/commands/psync.rs
+++ src/commands/psync.rs
@@ -1,4 +1,5 @@
 use crate::commands::resp;
+use crate::rdb::rdb;
 use crate::store::db;
 use std::io::Write;
 use std::net::TcpStream;
@@ -9,6 +10,14 @@ pub fn handler(
     stream: &mut TcpStream,
     _db: &Arc<db::DB>,
 ) -> std::io::Result<()> {
-    let response = "+FULLRESYNC 75cd7bc10c49047e0d163660f3b90625b1af31dc 0\r\n".to_string();
-    stream.write_all(response.as_bytes())
+    let database = rdb::RDB::new();
+    let mut response = vec![];
+    response
+        .extend_from_slice("+FULLRESYNC 75cd7bc10c49047e0d163660f3b90625b1af31dc 0\r\n".as_bytes());
+    if let Ok(rdb_content) = database.get() {
+        response.extend_from_slice(&rdb_content);
+    } else {
+        println!("---------- Error decoding RDB content ----------");
+    }
+    stream.write_all(&response)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::sync::Arc;
 use std::thread;
 
 mod commands;
+mod rdb;
 mod slave;
 mod store;
 
